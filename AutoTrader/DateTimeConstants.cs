4a097547e755487b42edf5defb285ccc0d9afd6b
Kiyoaki Tsurutani <kiyoaki.tsurutani@gmail.com>
2017-11-27 17:59:13 +0900

    Remove unused code

--- AutoTrader/DateTimeConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AutoTrader
-{
-    internal class DateTimeConstants
-    {
-        internal static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-    }
-}
--- AutoTrader/Extensions/TaskExtensions.cs
+++ AutoTrader/Extensions/TaskExtensions.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using NLog;
 
 namespace AutoTrader.Extensions
 {
